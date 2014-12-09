a59d7e0375dcab8d0171271b8d54d4f2ec4abcf7
Cedric Dumoulin <cedric.dumoulin@lifl.fr>
2014-12-09 11:31:29 +0100

    First release of simple app

---
+++ simpleapp/src/main/java/ipint14/tests/maven/simpleapp/App.java
@@ -0,0 +1,13 @@
+package ipint14.tests.maven.simpleapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
