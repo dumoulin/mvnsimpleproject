f52056634d477c917eb0365ce089dc5d68ad01df
Cedric Dumoulin <cedric.dumoulin@lifl.fr>
2014-12-15 23:04:30 +0100

    4 release of simple app

--- simpleapp/src/main/java/ipint14/tests/maven/simpleapp/App.java
+++ simpleapp/src/main/java/ipint14/tests/maven/simpleapp/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World 3 !" );
-        System.out.println( "Hello World 4 !" );
+
     }
 }
